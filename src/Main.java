a1cb0e6fe4f176512c24cac4e9b15d35c078dfb5
Vitalii <86424813+VitalikMk@users.noreply.github.com>
2024-02-07 21:34:42 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    static int staticDemo;
+
+    public static void main(String[] args) {
+        double i = 10;
+        double n = ++i % 5;
+        System.out.println(n);
+
+        /*double result = 3+10;
+        result++;*/
+
+
+        /*int anArray[] = new int[10];
+        anArray[0] = 100;
+        anArray[1] = 200;*/
+
+        /*int testvar= 0xFFFFFFFF;*/
+
+
+    }
+}
